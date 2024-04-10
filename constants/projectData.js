a00ee413029798c2833e3b25143c593716f36ce9
Vignesh Murugan <vigmurug@gmail.com>
2024-04-10 18:42:58 +0530

    updated: added data for project cards

---
+++ constants/projectData.js
@@ -0,0 +1,20 @@
+const projectData = [
+    {   
+        id: 1,
+        title: "SEAS Tech",
+        description: "A leading provider of innovative IoT solutions, Automation and Solar energy management for industrial and commercial purposes.",
+        liveLink: "https://seas-mu.vercel.app/",
+        githubLink: "https://github.com/webdev-vignesh/SEAS",
+        image: "/seastech.png"
+    },
+    {   
+        id: 2,
+        title: "Prime Plots",
+        description: "Real Estate Marketplace web application. Whether you are looking for your dream home, an investment property, or a commercial space, we got you covered.",
+        liveLink: "https://real-estate-app-d0v8.onrender.com/",
+        githubLink: "https://github.com/webdev-vignesh/prime-plots",
+        image: "/primeplots.png"
+    },
+]
+
+export default projectData;
