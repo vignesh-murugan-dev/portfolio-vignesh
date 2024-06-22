69dbe100085e3925a4056b29b1cba3759998efd1
Vignesh Murugan <vigmurug@gmail.com>
2024-06-22 19:25:05 +0530

    deleting old portfolio

--- components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-    return (
-        <div className='text-xl font-mono'><span className='underline'>Vignesh</span>&nbsp;<span className='text-gray-400'>Murugan</span></div>
-    )
-}
-
-export default Navbar
